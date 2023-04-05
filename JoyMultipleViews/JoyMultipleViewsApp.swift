4b4b7fe2e710a0b2e424a1205a0b8d6c145d71a1
Emma Finkel <emma.finkel@gmail.com>
2023-04-05 09:41:26 -0400

    Initial Commit

---
+++ JoyMultipleViews/JoyMultipleViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyMultipleViewsApp.swift
+//  JoyMultipleViews
+//
+//  Created by scholar on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct JoyMultipleViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
